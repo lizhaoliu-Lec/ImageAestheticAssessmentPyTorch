bad36cbdbdd8716870b06e88ac8cb7f64488eff8
lizhaoliu_Lec <2524463910@qq.com>
2021-04-18 00:08:02 +0800

    new features: add lr_scheduler

---
+++ lr_scheduler/lr_scheduler.py
@@ -0,0 +1,19 @@
+import torch.optim.lr_scheduler
+
+from lr_scheduler import LRSchedulerFactory
+
+
+@LRSchedulerFactory.register('ConstantLRScheduler')
+class ConstantLRScheduler(torch.optim.lr_scheduler._LRScheduler):
+    def get_lr(self):
+        return [group['lr'] for group in self.optimizer.param_groups]
+
+
+@LRSchedulerFactory.register('MultiStepLR')
+class MultiStepLR(torch.optim.lr_scheduler.MultiStepLR):
+    ...
+
+
+@LRSchedulerFactory.register('ExponentialLR')
+class ExponentialLR(torch.optim.lr_scheduler.ExponentialLR):
+    ...
