d547871249edb8f96e70b4a631565382be7a3ae9
lizhaoliu_Lec <2524463910@qq.com>
2021-04-26 19:58:22 +0800

    enhancement: 1. add freeze resnet bn option 2. add new experimental config

--- model/resnet_based.py
+++ model/resnet_based.py
@@ -6,6 +6,10 @@ from torchvision.models import resnet50
 from torchvision.models import resnet101
 from torchvision.models import resnet152
 
+from model.utils import freeze_bn, name_size_grad
+
+import logging
+
 
 class ResNetBase(nn.Module):
     NAME_2_RESNET = {
@@ -13,7 +17,7 @@ class ResNetBase(nn.Module):
         for _resnet in [resnet18, resnet34, resnet50, resnet101, resnet152]
     }
 
-    def __init__(self, resnet_name='resnet50', pretrained=True, **kwargs):
+    def __init__(self, resnet_name='resnet50', pretrained=True, bn_frozen=False, **kwargs):
         super().__init__()
         resnet = self.NAME_2_RESNET[resnet_name](pretrained=pretrained, **kwargs)
         self.conv1 = resnet.conv1
@@ -25,6 +29,9 @@ class ResNetBase(nn.Module):
         self.layer3 = resnet.layer3
         self.layer4 = resnet.layer4
         self.last_dim = 512 if resnet_name in ['resnet18', 'resnet34'] else 2048
+        if bn_frozen:
+            logging.info("The BN in ResNetBase is frozen")
+            freeze_bn(self)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -42,14 +49,24 @@ class ResNetBase(nn.Module):
 if __name__ == '__main__':
     import torch
 
-    x = torch.randn((5, 3, 224, 224))
-    model = ResNetBase(resnet_name='resnet34', pretrained=False)
-    print("===> x.size() ", x.size())
-    out = model(x)
-    print("===> out.size() ", out.size())
 
-    x = x.cuda()
-    model.cuda()
-    print("===> x.size() ", x.size())
-    out = model(x)
-    print("===> out.size() ", out.size())
+    def run_size():
+        x = torch.randn((5, 3, 224, 224))
+        model = ResNetBase(resnet_name='resnet34', pretrained=False)
+        print("===> x.size() ", x.size())
+        out = model(x)
+        print("===> out.size() ", out.size())
+
+        x = x.cuda()
+        model.cuda()
+        print("===> x.size() ", x.size())
+        out = model(x)
+        print("===> out.size() ", out.size())
+
+
+    def run_freeze_bn():
+        model = ResNetBase(resnet_name='resnet34', pretrained=False, bn_frozen=True)
+        name_size_grad(model)
+
+
+    run_freeze_bn()
